5dc8a97c89312c58df4aa4c097a26a91d38ad1aa
Luscarvalho42 <lucasdec.felizardo@gmail.com>
2021-06-20 19:00:44 -0300

    Finaliza o estilo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="h-screen flex-row bg-gray-50">
-    <h1 class="text-center ">GERADOR DE POKÉMON ALEATÓRIO</h1>
-    <CardPokemon :listaPokemons="listaPokemons" v-if="carregou" />
+  <div class="h-screen bg-gray-50 flex justify-center items-center">
+    <div class="flex-row">
+      <h1 id="titulo" class="text-center mb-10 text-4xl font-black text-yellow-400">GERADOR DE<br>POKÉMON<br>ALEATÓRIO</h1>
+      <CardPokemon :listaPokemons="listaPokemons" v-if="carregou" />
+    </div>
   </div>
 </template>
 
@@ -33,3 +35,9 @@ export default {
   
 }
 </script>
+
+<style scoped>
+  #titulo {
+    text-shadow: 0 2px 2px rgba(0, 0, 0, 0.5);
+  }
+</style>
--- src/components/CardPokemon.vue
+++ src/components/CardPokemon.vue
@@ -1,30 +1,52 @@
 <template>
-  <div v-if="carregou" id="card" class="m-auto p-3 rounded-lg shadow" :class="'bg-'+cor+'-100'">
-    <img :alt="nome" :src="img" class="w-3/6 m-auto"/>
-    <h1 class="capitalize text-center text-4xl font-bold">
-      {{ nome }}
-    </h1>
+  <div  id="card" class="flex items-end m-auto p-3 rounded-lg shadow duration-300" :class="'bg-'+cor+'-100'">
+    <div v-if="carregou">
+      <img :alt="nome" :src="img" class="w-3/6 m-auto"/>
+      <h1 class="capitalize text-center text-4xl font-bold">
+        {{ nome }}
+      </h1>
 
-    <div>
-      <div v-for="tipo in tipos" :key="tipo">
-        <h3>{{tipo}}</h3>
+      <div class="m-5 flex justify-around">
+        <div v-for="tipo in tipos" :key="tipo" class="bg-white p-2 w-32 rounded-md">
+          <h3 class="text-center uppercase font-bold">{{tipo}}</h3>
+        </div>
       </div>
-    </div>
 
-    <div>
-      <div><p>HP<br>{{status.hp}}</p></div>
-      <div><p>VELOCIDADE<br>{{status.velocidade}}</p></div>
-      <div><p>ATAQUE<br>{{status.ataque}}</p></div>
-      <div><p>DEFESA<br>{{status.defesa}}</p></div>
-      <h3>Especial</h3>
-      <div><p>ATAQUE<br>{{status.especial.ataque}}</p></div>
-      <div><p>DEFESA<br>{{status.especial.defesa}}</p></div>
-    </div>
+      <div 
+        :class="'bg-'+cor+'-300'"
+        class="flex flex-wrap justify-center py-2 mb-5 rounded-md duration-300">
+          <div class="status">
+            <p>HP<br>{{status.hp}}</p>
+          </div>
 
-    <div
-      v-on:click="gerarPokemon"
-      class="bg-yellow-300 p-2 rounded-md text-gray-500 shadow cursor-pointer hover:bg-yellow-200 duration-300">
-      <h2 class="font-bold text-center">GERAR OUTRO POKÉMON</h2>
+          <div class="status">
+            <p>VELOCIDADE<br>{{status.velocidade}}</p>
+          </div>
+
+          <div class="status">
+            <p>ATAQUE<br>{{status.ataque}}</p>
+          </div>
+
+          <div class="status">
+            <p>DEFESA<br>{{status.defesa}}</p>
+          </div>
+
+          <div class="status">
+            <p>ATAQUE-ESPECIAL<br>{{status.especial.ataque}}</p>
+          </div>
+
+          <div class="status">
+            <p>DEFESA-ESPECIAL<br>{{status.especial.defesa}}</p>
+          </div>
+
+      </div>
+
+      <div
+        v-on:click="gerarPokemon"
+        :class="'bg-'+cor+'-700'"
+        class="p-2 rounded-md text-white shadow cursor-pointer duration-300">
+        <h2 class="font-bold text-center">GERAR OUTRO POKÉMON</h2>
+      </div>
     </div>
   </div>
 </template>
@@ -99,5 +121,10 @@ export default {
 <style scoped>
   #card {
     width: 300px;
+    height: 480px;
+  }
+  
+  .status {
+    @apply bg-white w-36 rounded-sm m-1 text-center;
   }
 </style>
